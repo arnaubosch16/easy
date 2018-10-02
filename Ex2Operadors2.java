e1758121d19b8221d4e59cafd386ce00dabc31f2
arnaubosch16 <43789927+arnaubosch16@users.noreply.github.com>
2018-10-02 18:30:42 +0200

    Add files via upload

---
+++ Ex2Operadors2.java
@@ -0,0 +1,15 @@
+public class Ex2Operadors2 {
+
+  public static void main(String[] args) {
+
+    String nomDia, nomMes;
+    int any, dia;
+
+    nomDia = "martes";
+    dia = 2;
+    nomMes = "Octubre";
+    any = 2018;
+    System.out.println(nomDia + " " + dia + " " + nomMes + " " + any);
+
+  }
+}
